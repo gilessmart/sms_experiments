a070033bc74448af9d1ccbe714d1c7138fcec8d0
gilessmart <3227792+gilessmart@users.noreply.github.com>
2026-08-16 12:39:30 +0100

    Extend tilemap analyser tool to show unique tilemap values This tells me what potential there is to compress the tilemap

--- tools/sms_tilemap_freq/sms_tilemap_analyser.py
+++ tools/sms_tilemap_freq/sms_tilemap_analyser.py
@@ -5,24 +5,35 @@ import argparse
 import pathlib
 import re
 
+def add_to_frequency_dict(dict: dict[int, int], val: int):
+    if val in dict:
+        dict[val] += 1
+    else:
+        dict[val] = 1
+
 parser = argparse.ArgumentParser()
 parser.add_argument("file_path", type=pathlib.Path)
 args = parser.parse_args()
 
 file_path = args.file_path
 
-reg = re.compile(r"\$([0-9a-z]{4})", re.I)
+tilemap_value_freqs = {}
 tile_pattern_freqs = {}
+
+reg = re.compile(r"\$([0-9a-z]{4})", re.I)
 with open(file_path) as f:
     while l := f.readline():
         for match in reg.finditer(l):
-            tile_pattern = int(match[1], 16) & 511
-            if tile_pattern in tile_pattern_freqs:
-                tile_pattern_freqs[tile_pattern] += 1
-            else:
-                tile_pattern_freqs[tile_pattern] = 1
+            tilemap_value = int(match[1], 16) 
+            add_to_frequency_dict(tilemap_value_freqs, tilemap_value)
+            tile_pattern = tilemap_value & 511
+            add_to_frequency_dict(tile_pattern_freqs, tile_pattern)
 
-print(f"key count: {len(tile_pattern_freqs)}")
+print(f"Unique tilemap values used: {len(tilemap_value_freqs)}")
+
+print(f"Unique patterns used: {len(tile_pattern_freqs)}")
+
+print("Infrequently used patterns:")
 for k, c in tile_pattern_freqs.items():
     if c < 3:
         print(f"{hex(k)}: {c}")
